86827d4be01c008154ac4262b73b659ef1d94db2
MagnetonIO <mlong@magneton.io>
2025-12-17 15:18:34 -0600

    Bump version to 0.1.13

--- src/contextfs/__init__.py
+++ src/contextfs/__init__.py
@@ -21,7 +21,7 @@ Example:
     ctx.recall("abc123")
 """
 
-__version__ = "0.1.11"
+__version__ = "0.1.13"
 
 from contextfs.core import ContextFS
 from contextfs.schemas import Memory, MemoryType, Namespace, Session
